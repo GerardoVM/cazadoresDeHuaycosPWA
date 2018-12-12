76df8da78eb12d9ed18f86150b0f8ab113ee1fcb
Gerardo Valencia <gerardo@Gerardos-MacBook-Pro.local>
2018-12-12 10:49:50 -0500

    so

--- main.go
+++ main.go
@@ -7,20 +7,35 @@ import (
 	"github.com/gin-gonic/autotls"
 )
 
-func main() {
+//func main() {
 
-	r := gin.Default()
-	r.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"PUT", "POST", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "content-type"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowAllOrigins: true,
-	}))
-	LinkVideosApi(r)
-	LinkAdminAPI(r)
-	LinkOpenAPI(r)
-	LinkAuthJWT(r) // Making the auth context, all above this will be restricted
-	LinkUsersHelper(r)
-	log.Fatal(autotls.Run(r,"citapp.tk"))
+	//r := gin.Default()
+	//r.Use(cors.New(cors.Config{
+		//AllowMethods:     []string{"PUT", "POST", "OPTIONS"},
+		//AllowHeaders:     []string{"Origin", "Content-Type", "content-type"},
+		//ExposeHeaders:    []string{"Content-Length"},
+		//AllowCredentials: true,
+		//AllowAllOrigins: true,
+	//}))
+	//LinkVideosApi(r)
+	//LinkAdminAPI(r)
+	//LinkOpenAPI(r)
+	//LinkAuthJWT(r) // Making the auth context, all above this will be restricted
+	//LinkUsersHelper(r)
+	//log.Fatal(autotls.Run(r,"citapp.tk"))
+//}
+
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Content-Type", "text/plain")
+    w.Write([]byte("This is an example server.\n"))
+    // fmt.Fprintf(w, "This is an example server.\n")
+    // io.WriteString(w, "This is an example server.\n")
+}
+
+func main() {
+    http.HandleFunc("/hello", HelloServer)
+    err := http.ListenAndServeTLS(":443", "cert.crt", "cert.key", nil)
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
 }
